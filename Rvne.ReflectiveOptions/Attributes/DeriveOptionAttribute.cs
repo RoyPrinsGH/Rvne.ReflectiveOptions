3418556d8ef1d8262eb1bdb38829667793785f79
RoyPrinsGH <royprinsgm@gmail.com>
2026-01-26 21:19:52 +0100

    Delete old reflection-based derivation attribute

--- Rvne.ReflectiveOptions/Attributes/DeriveOptionAttribute.cs
+++
@@ -1,7 +0,0 @@
-namespace Rvne.ReflectiveOptions.Attributes;
-
-public abstract class DerivedOptionAttribute<TOptions, TDerivationResult>(string derivationMethodName) : Attribute
-{
-    public string DerivationMethodName { get; } = derivationMethodName;
-    public abstract void Apply(TDerivationResult derivationResult, TOptions options);
-}
